ebf5aadf3f0b149584efc98d0dff0743ce6f89a2
MatthewJamesJenks <Jenm1_16@uni.worc.ac.uk>
2017-11-20 13:55:59 +0000

    fg

--- www/js/index.js
+++ www/js/index.js
@@ -6,39 +6,38 @@ $(document).on("pagecreate","#pageone",function(){
 
 function random()
 {
-
-  var rand = Math.round(Math.random() * 6);
-
-  switch(rand) {
-      case 1:
-          alert("possitive")
-          navigator.notification.beep(1);
-          break;
-      case 2:
-          alert("possitive")
-          navigator.notification.beep(1);
-          break;
-      case 3:
-          alert("possitive")
-          navigator.notification.beep(1);
-          break;
-      case 4:
-      alert("Negative");
-      navigator.vibrate([2000]);
-          break;
-      case 5:
-          alert("Negative");
-          navigator.vibrate([2000]);
-          break;
-      case 6:
-          alert("Negative");
-          navigator.vibrate([2000]);
-          break;
-      default:
-          navigator.vibrate([2000]);
-
-  }
-    alert(rand);
+  alert("test");
+  // var rand = Math.round(Math.random() * 6);
+  //
+  // switch(rand) {
+  //     case 1:
+  //         alert("possitive " + rand)
+  //         navigator.notification.beep(1);
+  //         break;
+  //     case 2:
+          // alert("possitive " + rand)
+  //         navigator.notification.beep(1);
+  //         break;
+  //     case 3:
+  //         alert("possitive " + rand)
+  //         navigator.notification.beep(1);
+  //         break;
+  //     case 4:
+  //         alert("Negative " + rand);
+  //         navigator.vibrate([2000]);
+  //         break;
+  //     case 5:
+  //         alert("Negative " + rand);
+  //         navigator.vibrate([2000]);
+  //         break;
+  //     case 6:
+  //         alert("Negative " + rand);
+  //         navigator.vibrate([2000]);
+  //         break;
+  //     default:
+  //         navigator.vibrate([2000]);
+  //
+  // }
 
 
 
