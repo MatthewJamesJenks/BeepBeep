c5acf473bb795f357c43643a9019166a9312aedb
MatthewJamesJenks <Jenm1_16@uni.worc.ac.uk>
2017-11-20 15:10:20 +0000

    Stuff with possition

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,69 @@
+var unixtime;
+var date;
+var st = false;
+var locationOptions = {
+maximumAge: 10000,
+timeout: 6000,
+enableHighAccuracy: true
+};
+var watchID = navigator.geolocation.watchPosition(
+success, fail, locationOptions);
+
 $(document).on("pagecreate","#pageone",function(){
   $('#submitButton').on("click", function(){
     random();
   });
+  $('#toggle').on("click", function(){
+    toggleStatus();
+  });
 });
+
+
+function toggleStatus()
+{
+console.log(st + " _1");
+  if (st)
+  {
+    console.log("true");
+      st = !st;
+      alert("Off");
+  }
+  else
+  {
+    console.log("false");
+      st = !st;
+      success(position);
+      alert("On");
+  }
+console.log(st + " _2");
+}
+
+function success(position)
+{
+//do something with the position
+ unixtime = new Date(position.timestamp);
+ date = unixtime.toDateString();
+
+ var time = position.timestamp;
+ var latitude = position.coords.latitude;
+
+	//OK. Now we want to update the display with the correct values
+    $('#time').val("Recieved data at " + time);
+    $('#lattext').val("Recieved data at " + latitude);
+
+
+
+
+}
+
+function fail(error)
+{
+//do something with the error
+}
 
 function random()
 {
-  // alert("test");
+
   var rand = Math.round(Math.random() * 6);
 
   switch(rand) {
@@ -36,20 +93,11 @@ function random()
           break;
       default:
           navigator.vibrate([2000]);
-
   }
-
-
-
 }
 
 function submitText()
 {
 	var text = $('#textinput').val();
 	alert(text);
-}
-
-function storeValue(key, value)
-{
-	//add some code sto store the key-value pair in persistant storage
 }
